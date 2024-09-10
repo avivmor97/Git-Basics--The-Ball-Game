ca5c9262902892bf9b0bd64f641ffb39e76c1800
avivmor97 <aviv.mor97@gmail.com>
2024-09-10 17:09:00 +0300

    Add a maxDiameter parameter to onBallClick()

--- JS/main.js
+++ JS/main.js
@@ -3,16 +3,19 @@ var currentWidth = 100
 var currentHeight = 100
 
 
-function onBallClick(event) {
+
+
+function onBallClick(event, maxDiameter) {
     var elBall = event.target
     currentWidth += getRandomInt(20, 60)
     currentHeight += getRandomInt(20, 60)
+
+    if (currentWidth >= maxDiameter || currentHeight >= maxDiameter) {
+        currentWidth = 100 
+        currentHeight = 100 
+    }
+
     elBall.style.width = currentWidth + 'px'
     elBall.style.height = currentHeight + 'px'
     elBall.style.backgroundColor = getRandomColor()
-
-    if (currentHeight > 400) {
-        currentWidth = 100
-        currentHeight = 100
-    }
 }
