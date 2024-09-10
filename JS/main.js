deeab4984437bbe64debcf84f992cbae35468a6b
avivmor97 <aviv.mor97@gmail.com>
2024-09-10 17:28:38 +0300

    Add a third ball. When it is clicked, swap the colors and sizes of the first two balls

--- JS/main.js
+++ JS/main.js
@@ -5,17 +5,52 @@ var currentHeight = 100
 
 
 
+'use strict'
+
+var currentWidth1 = 100
+var currentHeight1 = 100
+var currentWidth2 = 100
+var currentHeight2 = 100
+
 function onBallClick(event, maxDiameter) {
     var elBall = event.target
-    currentWidth += getRandomInt(20, 60)
-    currentHeight += getRandomInt(20, 60)
 
-    if (currentWidth >= maxDiameter || currentHeight >= maxDiameter) {
-        currentWidth = 100 
-        currentHeight = 100 
+    
+    if (elBall.classList.contains('ball') || elBall.classList.contains('ball2')) {
+        // Increase size
+        currentWidth1 += getRandomInt(20, 60)
+        currentHeight1 += getRandomInt(20, 60)
+
+        // Check if size exceeds maxDiameter
+        if (currentWidth1 >= maxDiameter || currentHeight1 >= maxDiameter) {
+            currentWidth1 = 100
+            currentHeight1 = 100
+        }
+
+        elBall.style.width = currentWidth1 + 'px'
+        elBall.style.height = currentHeight1 + 'px'
+        elBall.style.backgroundColor = getRandomColor()
     }
+}
 
-    elBall.style.width = currentWidth + 'px'
-    elBall.style.height = currentHeight + 'px'
-    elBall.style.backgroundColor = getRandomColor()
+function swapBallProperties() {
+    var ball1 = document.querySelector('.ball')
+    var ball2 = document.querySelector('.ball2')
+
+    // Swap sizes
+    var tempWidth = ball1.style.width
+    var tempHeight = ball1.style.height
+    ball1.style.width = ball2.style.width
+    ball1.style.height = ball2.style.height
+    ball2.style.width = tempWidth
+    ball2.style.height = tempHeight
+
+    // Swap colors
+    var tempColor = ball1.style.backgroundColor
+    ball1.style.backgroundColor = ball2.style.backgroundColor
+    ball2.style.backgroundColor = tempColor
+}
+
+function onThirdBallClick() {
+    swapBallProperties()
 }
