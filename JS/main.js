48524862f2fa9f50acb1c4660d635f8b745f1255
avivmor97 <aviv.mor97@gmail.com>
2024-09-10 17:46:52 +0300

    Add a fifth ball. When it is clicked, the page's background color changes to a  random color

--- JS/main.js
+++ JS/main.js
@@ -8,7 +8,7 @@ var currentHeight2 = 100
 function onBallClick(event, maxDiameter) {
     var elBall = event.target
 
-    
+
     if (elBall.classList.contains('ball') || elBall.classList.contains('ball2')) {
         // Increase size
         currentWidth1 += getRandomInt(20, 60)
@@ -33,23 +33,23 @@ function reduceBallSizes() {
     var ball1Styles = getComputedStyle(ball1)
     var ball2Styles = getComputedStyle(ball2)
 
-  
+
     var width1 = parseFloat(ball1Styles.width)
     var height1 = parseFloat(ball1Styles.height)
     var width2 = parseFloat(ball2Styles.width)
     var height2 = parseFloat(ball2Styles.height)
 
-   
+
     var reduceAmount1 = getRandomInt(20, 60)
     var reduceAmount2 = getRandomInt(20, 60)
 
-    
+
     width1 = Math.max(100, width1 - reduceAmount1)
     height1 = Math.max(100, height1 - reduceAmount1)
     width2 = Math.max(100, width2 - reduceAmount2)
     height2 = Math.max(100, height2 - reduceAmount2)
 
-    
+
     ball1.style.width = width1 + 'px'
     ball1.style.height = height1 + 'px'
     ball2.style.width = width2 + 'px'
@@ -79,6 +79,14 @@ function onThirdBallClick() {
 }
 
 
-function onFourthBallClick(){
+function onFourthBallClick() {
     reduceBallSizes()
+}
+
+function onFifthBallClick(){
+    var elBody = document.querySelector('body')
+
+    elBody.style.backgroundColor = getRandomColor()
+
+
 }
