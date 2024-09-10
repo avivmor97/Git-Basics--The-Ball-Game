362104ebcd92a5c0639b82ab9a885c22a1274472
avivmor97 <aviv.mor97@gmail.com>
2024-09-10 17:02:51 +0300

    create second ball

--- JS/main.js
+++ JS/main.js
@@ -3,21 +3,16 @@ var currentWidth = 100
 var currentHeight = 100
 
 
-function onBallClick() {
-    var elBall = document.querySelector('.ball')
+function onBallClick(event) {
+    var elBall = event.target
     currentWidth += getRandomInt(20, 60)
     currentHeight += getRandomInt(20, 60)
     elBall.style.width = currentWidth + 'px'
     elBall.style.height = currentHeight + 'px'
     elBall.style.backgroundColor = getRandomColor()
 
-
-    if (currentHeight && currentHeight > 400) {
+    if (currentHeight > 400) {
         currentWidth = 100
         currentHeight = 100
     }
-
-
-
-
 }
