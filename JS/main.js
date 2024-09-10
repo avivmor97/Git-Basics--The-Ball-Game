099b8a2e4d3f59d7c1c7d896d9865a4e58ec2fa5
avivmor97 <aviv.mor97@gmail.com>
2024-09-10 16:07:38 +0300

    0.Change onBallClick() to increment the ball’s width & height by 50px

--- JS/main.js
+++ JS/main.js
@@ -1,8 +1,14 @@
 'use strict'
-
+var currentWidth = 100
+var currentHeight = 100
 
 
 function onBallClick(){
-    console.log('hi')
+    var elBall = document.querySelector('.ball')
+    currentWidth += 50
+    currentHeight += 50
+    elBall.style.width = currentWidth + 'px'
+    elBall.style.height = currentHeight + 'px'
+   
 
 }
